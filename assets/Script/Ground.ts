01d626de65b4a27e45e64f4d92e8dc49c1a841ff
Dmytro Grabko <dmitrygr@2kgroup.com>
2019-01-03 19:25:54 +0200

    ground movements issue fix

--- assets/Script/Ground.ts
+++ assets/Script/Ground.ts
@@ -6,24 +6,21 @@ export default class Ground extends cc.Component {
     @property
     speed: number = 120;
 
-    // LIFE-CYCLE CALLBACKS:
-
     onLoad () {
-	    cc.director.getCollisionManager().enabledDebugDraw = true;
 	    cc.director.getCollisionManager().enabled = true;
-	    cc.director.getCollisionManager().enabledDrawBoundingBox = true;
     }
 
     //start () {}
 
     public update (dt) {
         this.node.setPosition( this.node.position.x -= this.speed * dt, this.node.position.y);
-        if( this.node.position.x <= -(this.node.width) )
-            this.node.setPosition(this.node.width - 1, this.node.position.y);
+        if( this.node.position.x <= -(this.node.width) ){
+	        this.node.setPosition(2 * this.node.width + this.node.position.x, this.node.position.y);
+        }
+
     }
 
 	public onCollisionEnter(other, self) {
-		console.log("Currently colliding");
 		cc.game.pause();
 	}
 }
