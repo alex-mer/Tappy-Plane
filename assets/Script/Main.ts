4bb65abc774127c5156ebd33518ac3c8b6ff364a
Alex <alex.merckotan@gmail.com>
2019-01-03 15:13:10 +0200

    Add global touch

---
+++ assets/Script/Main.ts
@@ -0,0 +1,22 @@
+const {ccclass, property} = cc._decorator;
+
+import Plane from "./Plane";
+
+@ccclass
+export default class Main extends cc.Component {
+
+    @property(Plane)
+    plane: Plane = null;
+
+    public start (): void {
+        this.node.on(cc.Node.EventType.TOUCH_START, this.plane.onTap, this.plane);
+    }
+
+    public update (dt) {
+
+    }
+
+    public onDestroy() {
+        this.node.off(cc.Node.EventType.TOUCH_START, this.plane.onTap, this.plane);
+    }
+}
--- assets/Script/Plane.ts
+++ assets/Script/Plane.ts
@@ -15,20 +15,12 @@ export default class Plane extends cc.Component {
     @property(Number)
     rotation: number = 1;
 
-    public onLoad(): void {
-        this.node.on(cc.Node.EventType.TOUCH_START, this._onTap, this);
-    }
-
     public update(): void {
         this.node.y -= this.gravity;
         this.node.rotation += this.rotation;
     }
 
-    public onDestroy(): void {
-        this.node.off(cc.Node.EventType.TOUCH_START, this._onTap, this);
-    }
-
-    private _onTap(): void {
+    public onTap(): void {
         const rotation = cc.rotateTo(0.3, -25).easing(cc.easeCubicActionOut());
         const up = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
 
