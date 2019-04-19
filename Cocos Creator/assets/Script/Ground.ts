01823aba395e9e575f93e7f458209f9ac619633e
unknown <alex.merckotan@gmail.com>
2019-04-19 15:15:29 +0300

    Small fix

--- Cocos Creator/assets/Script/Ground.ts
+++ Cocos Creator/assets/Script/Ground.ts
@@ -1,27 +1,16 @@
 const {ccclass, property} = cc._decorator;
 
-import Main from "./Main";
-
 @ccclass
 export default class Ground extends cc.Component {
-    @property(Main)
-    main: Main = null;
-
     @property
-    speed: number = 120;
+    public speed: number = 120;
 
-    onLoad () {
-	    cc.director.getCollisionManager().enabled = true;
-    }
+    private _gap: number = 100;
 
-    public onUpdate (dt) {
-        this.node.setPosition( this.node.position.x -= this.speed * dt, this.node.position.y);
-        if( this.node.position.x <= -(this.node.width) ) {
+    public onUpdate (dt: number) {
+        this.node.setPosition(this.node.position.x -= this.speed * dt, this.node.position.y);
+        if( this.node.position.x <= -(this.node.width + this._gap) ) {
 	        this.node.setPosition(2 * this.node.width + this.node.position.x, this.node.position.y);
         }
-    }
-
-	public onCollisionEnter() {
-        this.main.gameOver();
     }
 }
--- Cocos Creator/assets/Script/Rock.ts
+++ Cocos Creator/assets/Script/Rock.ts
@@ -1,45 +1,43 @@
-const {ccclass, property} = cc._decorator;
-
 import Main from "./Main";
+
+const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class Ground extends cc.Component {
-    @property(Main)
-    main: Main = null;
+    @property(cc.Node)
+    public main: cc.Node = null;
 
     @property
-    speed: number = 120;
+    public speed: number = 120;
 
-    private _startX: number = 0;
-    private _startY: number = 0;
+    @property
+    public startPosition: number = 1100;
+
+    @property
+    public endPosition: number = -700;
+
+    private _scorePosition: number = -200;
     private _active: boolean =  true;
 
-    onLoad () {
-        cc.director.getCollisionManager().enabled = true;
-        
-        this._startX = this.node.position.x;
-        this._startY = this.node.position.y;
-    }
-
-    public onUpdate (dt) {
+    public onUpdate (dt: number) {
         this.node.setPosition(this.node.position.x -= this.speed * dt, this.node.position.y);
-        if(this.node.position.x < -650) {
-            this.node.setPosition(1100, this._startY);
+        if(this.node.position.x < this.endPosition) {
+            this.node.setPosition(this.startPosition, this._getRandomY());
             this._active = true;
         }
         
-        if(this._active && this.node.position.x < -90) {
-            this.main.updateScore();
+        if(this._active && this.node.position.x < this._scorePosition) {
+            this.main.getComponent(Main).updateScore();
             this._active = false;
         }
     }
-
-	public onCollisionEnter() {
-        this.main.gameOver();
-    }
     
     public toStart(): void {
-        this.node.setPosition(this._startX, this._startY);
+        this.node.setPosition(this.startPosition, this._getRandomY());
         this._active = true;
+    }
+
+    private _getRandomY(): number {
+        return (Math.random() * 300) - 150;
     }
 }
