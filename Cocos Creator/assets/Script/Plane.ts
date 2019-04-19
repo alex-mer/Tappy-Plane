6cd62141289314ba8ea8031eb57b5eb76a8719fd
unknown <alex.merckotan@gmail.com>
2019-04-19 15:14:59 +0300

    Change rotation to angle

--- Cocos Creator/assets/Script/Plane.ts
+++ Cocos Creator/assets/Script/Plane.ts
@@ -2,22 +2,23 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class Plane extends cc.Component {
+    @property
+    public jumpHeight: number = 40;
 
-    @property(Number)
-    jumpHeight: number = 40;
+    @property
+    public jumpDuration: number = 0.3;
 
-    @property(Number)
-    jumpDuration: number = 0.3;
+    @property
+    public gravity: number = 2;
 
-    @property(Number)
-    gravity: number = 2;
+    @property
+    public rotation: number = 1;
 
-    @property(Number)
-    rotation: number = 1;
+    public onCollisionEnter: Function;
 
     public onUpdate(): void {
 	    this.node.y -= this.gravity;
-        this.node.rotation += this.rotation;
+        this.node.angle -= this.rotation;
     }
 
     public stopActions(): void {
@@ -25,10 +26,12 @@ export default class Plane extends cc.Component {
     }
 
     public onTap(): void {
-	    const rotation = cc.rotateTo(0.3, -25).easing(cc.easeCubicActionOut());
+	    const rotation = cc.rotateTo(0.3, 25).easing(cc.easeCubicActionOut());
         const up = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
 
         this.node.runAction(rotation);
         this.node.runAction(up);
+
+        this.getComponent(cc.AudioSource).play();
     }
 }
