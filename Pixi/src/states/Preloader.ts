88200a18f899f04ca035d7b2894cf6dd3b164828
unknown <alex.merckotan@gmail.com>
2019-05-06 14:08:11 +0300

    Add audio

--- Pixi/src/states/Preloader.ts
+++ Pixi/src/states/Preloader.ts
@@ -3,13 +3,25 @@ import StateBase from "./../framework/state/StateBase";
 
 export default class Preloader extends StateBase {
   public onCreate(): void {
-    const loading = new Text("0")
+    const loading = new Text("0", "kenvector")
       .addStyle(13)
       .addPosition(this.centerX, this.centerY)
       .addToStage(this, this.foregroundGroup);
     loading.style.align = "center";
 
     this.app.load.sprite(["static/img/atlas.json"]);
+    this.app.load.audio("crash", [
+      "static/audio/crash.ogg",
+      "static/audio/crash.mp4"
+    ]);
+    this.app.load.audio("music", [
+      "static/audio/music.ogg",
+      "static/audio/music.mp4"
+    ]);
+    this.app.load.audio("tap", [
+      "static/audio/tap.ogg",
+      "static/audio/tap.mp4"
+    ]);
     this.app.load.onLoad((progress: number) => {
       loading.text = progress + "%";
     });
