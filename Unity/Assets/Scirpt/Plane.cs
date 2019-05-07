668b06c8a5e7cb73eff88767041e4a87edbb1e5b
unknown <alex.merckotan@gmail.com>
2019-05-07 11:43:13 +0300

    Add tap

---
+++ Unity/Assets/Scirpt/Plane.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour {
+    public void onTap() {
+        Debug.Log("tap");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 250f));
+    }
+}
