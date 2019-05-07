fb8fec787b6489ed07a7fd9c422b41b4fbf2bbd5
unknown <alex.merckotan@gmail.com>
2019-05-07 18:38:26 +0300

    Add GameOver

--- Unity/Assets/Scirpt/Game.cs
+++ Unity/Assets/Scirpt/Game.cs
@@ -1,24 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
-    public GameObject startScreen;
+    public GameObject[] startScreen;
     public GameObject plane;
     public GameObject score;
-    public GameObject rock;
+    public GameObject topScore;
+    public GameObject rockTop;
+    public GameObject rockBottom;
+    public AudioClip tapAudioClip;
+    public AudioClip crashAudioClip;
 
     void Start() {
         Config config = new Config();
-        /*GameObject quad;
-        quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        quad.transform.rotation = Quaternion.Euler(90, 0, 0);
-        quad.transform.localScale = new Vector3(8, 8, 1);*/
     }
 
     void Update() {
         if (Config.state == "game") {
-            if(rock.transform.position.x.ToString("0.0") == "0,0") {
+            if(rockTop.transform.position.x.ToString("0.0") == "0,0") {
                 score.GetComponent<Score>().updateScore();
             }
 
@@ -30,12 +31,50 @@ public class Game : MonoBehaviour {
                 _startGame();
             }
         }
+
+        if (Input.GetMouseButtonUp(0)) {
+            GetComponent<AudioSource>().PlayOneShot(tapAudioClip, 3);
+        }
     }
 
-    void _startGame() {
-        startScreen.SetActive(false);
-        plane.GetComponent<Rigidbody2D>().simulated = true;
+    public void endGame() {
+        Config.state = "end";
+        score.SetActive(false);
+        topScore.SetActive(true);
 
+        topScore.GetComponent<Text>().text = "Top score:" + score.GetComponent<Score>().getTopScore().ToString();
+
+        plane.GetComponent<Rigidbody2D>().simulated = false;
+        plane.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+        plane.transform.position = new Vector3(0, 0, plane.transform.position.z);
+
+        rockTop.GetComponent<Rock>().toStart();
+        rockBottom.GetComponent<Rock>().toStart();
+
+        GetComponent<AudioSource>().PlayOneShot(crashAudioClip);
+
+        _animation(startScreen, 1f);
+        _vibration();
+    }
+
+    private void _startGame() {
         Config.state = "game";
+        score.SetActive(true);
+        topScore.SetActive(false);
+        score.GetComponent<Score>().Start();
+        plane.GetComponent<Rigidbody2D>().simulated = true;
+        _animation(startScreen, 0f);
+    }
+
+    private void _animation(GameObject []objects, float alpha) {
+        foreach (GameObject i in objects) {
+            iTween.ColorTo(i, iTween.Hash("a", alpha, "time", 0.3f));
+        }
+    }
+
+    private void _vibration() {
+        iTween.MoveTo(gameObject, iTween.Hash("x", 2.5f, "delay", 0f));
+        iTween.MoveTo(gameObject, iTween.Hash("x", -2.5f, "delay", 0.1f));
+        iTween.MoveTo(gameObject, iTween.Hash("x", 0f, "delay", 0.2f));
     }
 }
