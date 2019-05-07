c8f1cfee4faaee8c2a0116b323b902b276e22e76
unknown <alex.merckotan@gmail.com>
2019-05-07 18:38:33 +0300

    Add Colision

--- Unity/Assets/Scirpt/Plane.cs
+++ Unity/Assets/Scirpt/Plane.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Plane : MonoBehaviour {
+    public GameObject game;
+    
+    public void OnCollisionEnter2D() {
+        game.GetComponent<Game>().endGame();
+    }
+
     public void onTap() {
-        Debug.Log("tap");
         GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
         GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 250f));
     }
