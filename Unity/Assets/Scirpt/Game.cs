de44f7e08f64490e969ec01664dba9ed4cca6a7a
unknown <alex.merckotan@gmail.com>
2019-05-07 11:44:06 +0300

    Add game loop

---
+++ Unity/Assets/Scirpt/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public GameObject startScreen;
+    public GameObject plane;
+
+    void Start() {
+        Debug.Log("zalupa");
+        Config config = new Config();
+        /*GameObject quad;
+        quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        quad.transform.rotation = Quaternion.Euler(90, 0, 0);
+        quad.transform.localScale = new Vector3(8, 8, 1);*/
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            if (Config.state == "game") {
+                plane.GetComponent<Plane>().onTap();
+            } else {
+                _startGame();
+            }
+        }
+    }
+
+    void _startGame() {
+        startScreen.SetActive(false);
+        plane.GetComponent<Rigidbody2D>().simulated = true;
+
+        Config.state = "game";
+    }
+}
