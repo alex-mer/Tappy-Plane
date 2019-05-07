c53be9085c7bbe25bec4815a2ff17b3e812e73f0
unknown <alex.merckotan@gmail.com>
2019-05-07 11:43:37 +0300

    Add move

---
+++ Unity/Assets/Scirpt/Ground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+    public float speed;
+    public float gap;
+
+    void Update() {
+        if (Config.state == "game") {
+            float width = GetComponent<Renderer>().bounds.size.x;
+            
+            if (transform.position.x < -(width + gap)) {
+                transform.position = new Vector3((2 * width + transform.position.x), transform.position.y, transform.position.z);
+            }
+
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Unity/Assets/Scirpt/Rock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+    public float speed;
+    public float startPosition;
+    public float endPosition;
+
+    void Update() {
+        if (Config.state == "game") {
+            if (transform.position.x < endPosition) {
+                toStart();
+            }
+
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+    }
+
+    void toStart() {
+        transform.position = new Vector3(startPosition, transform.position.y, transform.position.z);
+    }
+}
