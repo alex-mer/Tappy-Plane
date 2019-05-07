35907bb2d6b7827ad31935ceb242cd2dea7e680e
unknown <alex.merckotan@gmail.com>
2019-05-07 11:43:51 +0300

    Init

---
+++ Unity/Assets/Scirpt/Config.cs
@@ -0,0 +1,12 @@
+﻿public class Config {
+   public static Config getInstance() {
+        if (Config._instance == null) {
+            Config._instance = new Config();
+        }
+
+        return Config._instance;
+    }
+
+    public static string state = "start";
+    private static Config _instance;
+}
