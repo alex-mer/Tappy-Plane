09b4ee6fc90ab461fef9e90eb9a125d1f058523d
unknown <alex.merckotan@gmail.com>
2019-05-07 18:37:48 +0300

    Add Top Score

--- Unity/Assets/Scirpt/Score.cs
+++ Unity/Assets/Scirpt/Score.cs
@@ -5,9 +5,23 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
     private int _score = 0;
+    private int _topScore = 0;
+
+    public void Start() {
+        _score = 0;
+        GetComponent<Text>().text = _score.ToString();
+    }
 
     public void updateScore() {
         _score++;
         GetComponent<Text>().text = _score.ToString();
+
+        if (_score > _topScore) {
+            _topScore = _score;
+        }
+    }
+
+    public int getTopScore() {
+        return _topScore;
     }
 }
